94ecb769102f48b4fe8202cdae0ce7ecdf41967d
Daniel Eby <ebydj1@gmail.com>
2013-06-09 12:26:06 -0400

    Added rudimentary date calculation class
    Only works between 1900 and 2100
    Can detect invalid dates
    Can return proper weekday for any date
    
    Further refined .gitignore

---
+++ Dt.cpp
@@ -0,0 +1,37 @@
+#include "Dt.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+short doy[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+inline bool leap(int y) { return !(y % 400) || !(y % 4) && (y % 100); }
+inline int nmult(int start, int end, int factor)
+{
+  return (end/factor*factor - start/factor*factor)/factor;
+}
+
+inline int nleap(int start, int end)
+{
+  return nmult(start, end, 4) - nmult(start, end, 100) + nmult(start, end, 400)
+       + (leap(start) ? 1 : 0);
+}
+
+Err Dt::dtSet(int y_, int m_, int d_)
+{
+  --d_;
+  if (y_ < 1900 || y_ > 2100)
+    return Err(dt_set, invalid_yr);
+  if ((!leap(y_) || m_ != Feb) && d_ >= doy[m_ + 1] - doy[m_]
+    || leap(y_) && m_ == Feb && d_ >= 29
+    || d_ < 0)
+    return Err(dt_set, invalid_day);
+  y = y_;
+  m = m_;
+  d = d_;
+  return Err();
+}
+
+int Dt::weekday()
+{
+  return (Mon + y-1900 + nleap(1900, m > Feb ? y : y-1) + doy[m] + d) % 7;
+}
---
+++ Dt.h
@@ -0,0 +1,33 @@
+enum Func
+{
+  goodf = 0, dt_set
+};
+
+enum Type
+{
+  goodt = 0, invalid_yr, invalid_day
+};
+
+const int Jan = 0, Feb = 1, Mar = 2, Apr = 3, May = 4, Jun = 5, Jul = 6,
+          Aug = 7, Sep = 8, Oct = 9, Nov = 10, Dec = 11;
+
+const int Sun = 0, Mon = 1, Tue = 2, Wed = 3, Thu = 4, Fri = 5, Sat = 6;
+
+struct Err
+{
+  Func func;
+  Type type;
+  Err(Func func_, Type type_) : func(func_), type(type_) {}
+  Err() : func(goodf), type(goodt) {}
+};
+
+class Dt
+{
+public:
+  Err dtSet(int y_, int m_, int d_);
+  int weekday();
+private:
+  int y;
+  int m;
+  int d;
+};
